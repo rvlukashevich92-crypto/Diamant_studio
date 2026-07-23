8000e11d829b31f0383a6a21510d82ff88c507b9
Roman <rvlukashevich92@google.com>
2026-07-23 19:59:51 +0300

    Project

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib import messages
@@ -6,6 +6,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils import timezone
 from .forms import ClientRegistrationForm
 from appoinments.models import Application
+from appoinments.forms import ApplicationForm
 
 def login_view(request):
     if request.method == "POST":
@@ -59,6 +60,31 @@ def register_view(request):
         form = ClientRegistrationForm()
         
     return render(request, "users/register.html", {"form": form})
+
+@login_required
+def appointment_cancel(request, pk):
+    appointment = get_object_or_404(Application, pk=pk, user=request.user)
+
+    if request.method == "POST":
+        appointment.delete()
+        messages.success(request, "Запись успешно отменена.")
+
+    return redirect('profile_dashboard')
+
+@login_required
+def appointment_update(request, pk):
+    appointment = get_object_or_404(Application, pk=pk, user=request.user)
+
+    if request.method == "POST":
+        form = ApplicationForm(request.POST, instance=appointment)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Запись успешно изменена!")
+            return redirect("profile_dashboard")
+    else:
+        form = ApplicationForm(instance=appointment)
+
+    return render(request, "appointment.html", {"form":form, "is_edit": True})
 
 
 
